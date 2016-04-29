2144e866ed6383f5b3714bfc488884b10c275a9e
Dmitry Rocha <dmitryrck@gmail.com>
2016-04-29 18:42:49 -0300

    Update mentions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     text.gsub(%r_@[a-zA-Z0-9]*_) { |name|
       name_without_mention = name.gsub('@', '')
 
-      %[<a href="http://www.twitter.com/#{name_without_mention}">#{name_without_mention}</a>]
+      %[<a href="http://www.twitter.com/#{name_without_mention}">@#{name_without_mention}</a>]
     }.html_safe
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe ApplicationHelper do
   context '#replace_mentions' do
     it 'returns link with username' do
-      expect(replace_mentions('Hi @twitter!')).to eq 'Hi <a href="http://www.twitter.com/twitter">twitter</a>!'
+      expect(replace_mentions('Hi @twitter!')).to eq 'Hi <a href="http://www.twitter.com/twitter">@twitter</a>!'
     end
   end
 end
