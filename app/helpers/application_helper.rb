d48eb2b2c4fa3e2fb913e4d53fd056ea1f20ef6a
Dmitry Rocha <dmitryrck@gmail.com>
2016-04-29 18:02:42 -0300

    Create replace_mentions helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def replace_mentions(text)
+    text.gsub(%r_@[a-zA-Z0-9]*_) { |name|
+      name_without_mention = name.gsub('@', '')
+
+      %[<a href="http://www.twitter.com/#{name_without_mention}">#{name_without_mention}</a>]
+    }.html_safe
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe ApplicationHelper do
+  context '#replace_mentions' do
+    it 'returns link with username' do
+      expect(replace_mentions('Hi @twitter!')).to eq 'Hi <a href="http://www.twitter.com/twitter">twitter</a>!'
+    end
+  end
+end
