edc624fdecdc9e7e40a1b1d950ad1b024502cd0f
Dmitry Rocha <dmitryrck@gmail.com>
2016-04-29 21:19:58 -0300

    Convert links from tweet text to clickable links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,21 @@
 module ApplicationHelper
+  def formatted_tweet_text(text)
+    replace_mentions(replace_links(text)).html_safe
+  end
+
+  private
+
   def replace_mentions(text)
     text.gsub(%r_@[a-zA-Z0-9]*_) { |name|
       name_without_mention = name.gsub('@', '')
 
       %[<a href="http://www.twitter.com/#{name_without_mention}">@#{name_without_mention}</a>]
-    }.html_safe
+    }
+  end
+
+  def replace_links(text)
+    text.gsub(%r_http(s)?\:\/\/[a-z0-9\.]*_) { |link|
+      %[<a href="#{link}">#{link}</a>]
+    }
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  context '#replace_mentions' do
+  context '#formatted_tweet_text' do
     it 'returns link with username' do
-      expect(replace_mentions('Hi @twitter!')).to eq 'Hi <a href="http://www.twitter.com/twitter">@twitter</a>!'
+      expect(formatted_tweet_text('Hi @twitter!')).to eq 'Hi <a href="http://www.twitter.com/twitter">@twitter</a>!'
+    end
+
+    it 'returns http link' do
+      expect(formatted_tweet_text('Go to http://example.de!')).to eq 'Go to <a href="http://example.de">http://example.de</a>!'
+    end
+
+    it 'returns https link' do
+      expect(formatted_tweet_text('Go to https://example.com!')).to eq 'Go to <a href="https://example.com">https://example.com</a>!'
     end
   end
 end
